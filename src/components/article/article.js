e3d1513b3a4948f6e5cb7176357f2cd2b200ad06
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2023-02-06 18:09:54 +0400

    finished blog and article components

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './article.css';
 
-const article = () => {
-  return <div>article</div>;
+const Article = ({ image, date, title }) => {
+  return (
+    <div className="gpt3-blog-container-article">
+      <div className="gpt3-blog-container-article-image">
+        <img src={image} alt="blog" />
+      </div>
+      <div className="gpt3-blog-container-article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read full Article</p>
+      </div>
+    </div>
+  );
 };
 
-export default article;
+export default Article;
--- src/containers/blog/blog.js
+++ src/containers/blog/blog.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
-const blog = () => {
-  return <div>blog</div>;
+const articleData = [
+  {
+    image: blog02,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+  },
+  {
+    image: blog03,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+  },
+  {
+    image: blog04,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+  },
+  {
+    image: blog05,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+  },
+];
+
+const Blog = () => {
+  return (
+    <div className="gpt3-blog section-padding" id="blog">
+      <div className="gpt3-blog-heading">
+        <h1 className="gradient-text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3-blog-container">
+        <div className="gpt3-blog-container-group-a">
+          <Article
+            image={blog01}
+            date={'Sep 26, 2021'}
+            title={'GPT-3 and Open  AI is the future. Let us exlore how it is?'}
+          />
+        </div>
+        <div className="gpt3-blog-container-group-b">
+          {articleData.map(({ image, date, title }, index) => {
+            return (
+              <Article
+                image={image}
+                date={date}
+                title={title}
+                key={index + new Date().toDateString()}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default blog;
+export default Blog;
