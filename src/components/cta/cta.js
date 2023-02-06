b579c80cc10cfa83d59b86b303bd8ae520d1b12b
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2023-02-06 13:31:53 +0400

    finished call to action/cta component

--- src/components/cta/cta.js
+++ src/components/cta/cta.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './cta.css';
 
-const cta = () => {
-  return <div>cta</div>;
+const Cta = () => {
+  return (
+    <div className="gpt3-cta">
+      <div className="gpt3-cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3-cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
-export default cta;
+export default Cta;
